a0d0e10730c7163045a450a7008f73d384a50f71
fazel <amirfazel45@gmail.com>
2021-07-27 19:29:13 +0430

    Initial commit

---
+++ src/sample/Controllers/MainMenu.java
@@ -0,0 +1,37 @@
+package sample.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MainMenu {
+
+    @FXML
+    private Pane mainMenuPane;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private Pane sidePane;
+
+    @FXML
+    private Button infoButton;
+
+    @FXML
+    void goToInfo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void goToSignUp(ActionEvent event) {
+
+    }
+
+}
