1d05ed896e198e334a06a8ce62d4774853ad9920
amirfazel <amirfazel45@gmail.com>
2021-07-27 10:07:13 -0700

    signUp page designed

---
+++ src/sample/Controllers/SignUp.java
@@ -0,0 +1,42 @@
+package sample.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class SignUp {
+    private final SceneLoader sceneLoader = new SceneLoader();
+
+    @FXML
+    private Pane SignUpPane;
+
+    @FXML
+    private Button SignUpButton;
+
+    @FXML
+    private Button goBackButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private PasswordField secPassField;
+
+    @FXML
+    void goToMain() {
+        sceneLoader.goToMainMenu(SignUpPane);
+    }
+
+    @FXML
+    void signUpUser(ActionEvent event) {
+
+    }
+
+}
