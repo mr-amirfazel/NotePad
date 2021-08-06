cd740cc8e879c58f84a4feeb947c43524a5beb7e
amirfazel <amirfazel45@gmail.com>
2021-08-06 16:22:07 +0430

    Info page added

--- src/sample/Controllers/MainMenu.java
+++ src/sample/Controllers/MainMenu.java
@@ -1,11 +1,18 @@
 package sample.Controllers;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -30,7 +37,27 @@ public class MainMenu implements Initializable {
 
     @FXML
     void goToInfo(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION)
+                ;
+        alert.setTitle("Info");
+        alert.setContentText("" +
+                "Note Pad (second version of ToDoList)\n" +
+                "implemented in java\n" +
+                "developed by AmirFazel Koozegar\n");
+        alert.setHeaderText("Note Pad");
 
+
+        VBox Pane = new VBox();
+        Pane.getChildren().add(new Label("Note Pad (second version of ToDoList)\n" +
+                "implemented in java\n" +
+                "developed by AmirFazel Koozegar\n"));
+        ImageView imageView = new ImageView(new Image("notepad.jpg"));
+        Pane.getChildren().add(imageView);
+
+        alert.getDialogPane().setContent(Pane);
+        alert.setResizable(true);
+
+        alert.show();
     }
 
     @FXML
